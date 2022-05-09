7a2894f4bb45c1dafa6a22155e560e1922ec0ccf
Alex <x3m.f@mail.ru>
2022-05-09 12:12:59 +0400

    feat: add CRA and dependencies

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.module.scss';
+import { TestComponent } from './components/testComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <TestComponent />
+    </div>
+  );
+}
+
+export default App;
