c1c0ff313d7c457780bfee701da08012765db12e
Max Tyshkevich <2842778@gmail.com>
2022-05-15 20:54:58 +0300

    feat: page registr and autorization

--- src/pages/PageAuthorization/Authorization.tsx
+++ src/pages/PageAuthorization/Authorization.tsx
@@ -1,68 +1,104 @@
 import { Link, useNavigate } from 'react-router-dom';
 import { FieldValues, useForm } from 'react-hook-form';
-import { useAppDispatch } from '../../store/hook/hook';
-import { login } from '../../store/authSlice/authSlice';
+import { useAppDispatch, useAppSelector } from '../../store/hook/hook';
+import { cleanMessage, login } from '../../store/authSlice/authSlice';
 
-import { Container, Box, FormLabel, TextField, Button } from '@mui/material';
-import { useState } from 'react';
+import styles from './Authorization.module.scss';
 
-export interface AuthorizationFields {
-  login: string;
-  password: string;
-}
+import {
+  Container,
+  Box,
+  FormLabel,
+  TextField,
+  Button,
+  CircularProgress,
+  Alert,
+} from '@mui/material';
+import { AccountIntrface } from '../../services/authorization/interface.account';
+
+import { useEffect } from 'react';
 
 const Authorization = () => {
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const { isLoading, message } = useAppSelector((state) => state.authReducer);
   const navigation = useNavigate();
 
   const dispatch = useAppDispatch();
 
-  const { register, handleSubmit } = useForm<AuthorizationFields>();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isValid },
+  } = useForm<AccountIntrface>({ mode: 'onChange' });
 
-  const onSubmit = async (data: AuthorizationFields) => {
+  const onSubmit = async (data: AccountIntrface) => {
     try {
-      setIsLoading(true);
       await dispatch(login(data)).unwrap();
       navigation('/main', { replace: true });
     } catch (error) {
-      console.log('result error: ', error);
+      console.log('eeerrror!!!');
     }
-
-    setIsLoading(false);
+    reset();
   };
+
+  useEffect(() => {
+    if (message) {
+      setTimeout(() => {
+        dispatch(cleanMessage());
+      }, 3000);
+    }
+  }, [dispatch, message]);
 
   return (
     <Container>
-      <form className="form" onSubmit={handleSubmit<FieldValues>(onSubmit)}>
-        <div className="form__title">
-          <h2>Авторизация</h2>
-          {isLoading && <p>....loading</p>}
+      <form className={styles.form} onSubmit={handleSubmit<FieldValues>(onSubmit)}>
+        {isLoading && (
+          <CircularProgress
+            sx={{
+              position: 'absolute',
+              left: '50%',
+              top: '50%',
+              transform: 'translate(-50%, -50%)',
+            }}
+          />
+        )}
+        <div className={styles.form__element}>
+          <h2 className={styles.form__title}>Авторизация</h2>
         </div>
-        <Box className="form__element">
-          <FormLabel htmlFor="login">Login:*</FormLabel>
+        <Box className={styles.form__element}>
+          <FormLabel htmlFor="login" className={styles.form__label}>
+            Login:*
+          </FormLabel>
           <TextField
             variant="outlined"
             type="text"
             {...register('login', { required: `reqeired field` })}
             id="login"
+            error={!!errors?.login}
             defaultValue={`user001`}
           />
+          <div className={styles.form__feedback}>{errors.login?.message}</div>
         </Box>
-        <Box className="form__element">
-          <FormLabel htmlFor="password">Password:*</FormLabel>
+        <Box className={styles.form__element}>
+          <FormLabel htmlFor="password" className={styles.form__label}>
+            Password:*
+          </FormLabel>
           <TextField
-            error={false}
             variant="outlined"
             type="password"
             {...register('password', { required: true })}
             id="password"
+            error={!!errors?.password}
             defaultValue={`userpass@123`}
           />
+          <div className={styles.form__feedback}>{errors.password?.message}</div>
         </Box>
-        <Button variant="contained" type="submit">
+        <Button variant="contained" type="submit" disabled={!isValid}>
           Submit
         </Button>
       </form>
+
+      {message && <Alert severity="error">{message}</Alert>}
 
       <Link to="/registration">Зарегистрироваться</Link>
     </Container>
--- src/pages/PageRegistration/PageRegistration.tsx
+++ src/pages/PageRegistration/PageRegistration.tsx
@@ -1,13 +1,103 @@
+import { Link, useNavigate } from 'react-router-dom';
+import { FieldValues, useForm } from 'react-hook-form';
+import { useAppDispatch, useAppSelector } from '../../store/hook/hook';
+
+import { Container, Box, FormLabel, TextField, Button, CircularProgress } from '@mui/material';
+import { AccountIntrface } from '../../services/authorization/interface.account';
+import { registerUser } from '../../store/authSlice/authSlice';
+
+import styles from './PageRegistration.module.scss';
+
 const PageRegistration = () => {
+  const { isLoading } = useAppSelector((state) => state.authReducer);
+  const navigation = useNavigate();
+
+  const dispatch = useAppDispatch();
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isValid },
+  } = useForm<AccountIntrface>({ mode: 'onChange' });
+
+  const onSubmit = async (data: AccountIntrface) => {
+    try {
+      await dispatch(registerUser(data)).unwrap();
+      navigation('/authorization', { replace: true });
+    } catch (error) {}
+    reset();
+  };
+
   return (
-    <div>
-      <h2>Тут будет регистрация: </h2>
-      <input type="text" />
-      <br />
-      <input type="text" />
-      <br />
-      <button>send</button>
-    </div>
+    <Container>
+      <form className={styles.form} onSubmit={handleSubmit<FieldValues>(onSubmit)}>
+        {isLoading && (
+          <CircularProgress
+            sx={{
+              position: 'absolute',
+              left: '50%',
+              top: '50%',
+              transform: 'translate(-50%, -50%)',
+            }}
+          />
+        )}
+        <div className={styles.form__element}>
+          <h2 className={styles.form__title}>Регистрация</h2>
+        </div>
+        <Box className={styles.form__element}>
+          <FormLabel htmlFor="name" className={styles.form__label}>
+            Name:*
+          </FormLabel>
+          <TextField
+            variant="outlined"
+            type="text"
+            {...register('name', {
+              required: `reqeired field`,
+              minLength: { value: 3, message: 'minimum 3 chair' },
+              pattern: {
+                value: /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/g,
+                message: 'using letter',
+              },
+            })}
+            id="name"
+            error={!!errors?.name}
+          />
+          <div className={styles.form__feedback}>{errors.name?.message}</div>
+        </Box>
+        <Box className={styles.form__element}>
+          <FormLabel htmlFor="login" className={styles.form__label}>
+            Login:*
+          </FormLabel>
+          <TextField
+            variant="outlined"
+            type="text"
+            {...register('login', { required: `reqeired field` })}
+            id="login"
+            error={!!errors?.login}
+          />
+          <div className={styles.form__feedback}>{errors.login?.message}</div>
+        </Box>
+        <Box className={styles.form__element}>
+          <FormLabel htmlFor="password" className={styles.form__label}>
+            Password:*
+          </FormLabel>
+          <TextField
+            variant="outlined"
+            type="password"
+            {...register('password', { required: true })}
+            id="password"
+            error={!!errors?.password}
+          />
+          <div className={styles.form__feedback}>{errors.password?.message}</div>
+        </Box>
+        <Button variant="contained" type="submit" disabled={!isValid}>
+          Submit
+        </Button>
+      </form>
+
+      <Link to="/authorization">авторизация</Link>
+    </Container>
   );
 };
 
