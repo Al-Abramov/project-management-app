22d869773569fdf8a9efc90f4ca5b3b01d1a6a7d
Alex <x3m.f@mail.ru>
2022-05-31 00:07:00 +0400

    fix: not found page

--- src/pages/PageNotFound/PageNotFound.tsx
+++ src/pages/PageNotFound/PageNotFound.tsx
@@ -1,13 +1,18 @@
+import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
-import { Container } from 'react-bootstrap';
+import style from './PageNotFound.module.scss';
 
 const PageNotFound = () => {
   return (
     <>
       <Header />
-      <Container>
-        <h2 className="text-center mt-3">Page Not Found 404</h2>
-      </Container>
+      <main className={style.notFound}>
+        <div className={style.title}>
+          <span className={style.big}>404</span>
+          <span>Page Not found</span>
+        </div>
+      </main>
+      <Footer />
     </>
   );
 };
